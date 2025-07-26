3ea9ce795ee8ffaecbb7db6b2041f2e6b996afea
mahidul-islam-v <mahidulislam.00713@example.com>
2025-07-26 14:48:05 +0600

    Palindrome Updadte

--- Projects/JS/Simple/03. Pal' In D Rome/script.js
+++ Projects/JS/Simple/03. Pal' In D Rome/script.js
@@ -1,13 +1,23 @@
 var textArea = document.getElementById('input')
 var result = document.getElementsByTagName('h3')[0]
 
+window.onload = textArea.value = ""
+
+document.addEventListener('keydown', function(pressed) {
+    if (pressed.key === 'Enter') {
+        check()
+    } else if (document.activeElement !== textArea) {
+        textArea.focus();
+    }
+})
+
 async function check() {
     let userInput = textArea.value;
     let reversedInput = userInput.split('').reverse().join('')
     let isValidWord = false
 
     if (!userInput) {
-        result.innerText = "Please enter a word."
+        result.innerText = `Type In A Word.`
         document.body.style.backgroundColor = "white"
     } else {
         result.innerHTML = "<span style='font-weight:normal;'>Please wait</span>"
@@ -20,17 +30,19 @@ async function check() {
         }
 
         if(!isValidWord) {
-            result.innerText = "Please enter a valid English word."
+            result.innerText = `${userInput} is not a valid English word.`
             document.body.style.backgroundColor = "#FFEF00"
         } else {
                 if (userInput === reversedInput) {
                 result.innerText = `${userInput} is a palindrome`
-                document.body.style.backgroundColor = "#00FA9A"
+                document.body.style.backgroundColor = "#0bda5aff"
             } else {
                 result.innerText = `${userInput} isn't a palindrome`
                 document.body.style.backgroundColor = "#ff3f35ff"
                 result.style.color = "#232020ff"
             }
         }
+
+        textArea.value = ""
     }
 }
